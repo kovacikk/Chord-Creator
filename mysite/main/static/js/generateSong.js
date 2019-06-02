ce659273f88966084e7224f40c07915c19ef2d74
kovacikk <kovacikkyl@gmail.com>
2019-06-02 12:52:34 -0400

    Fixed Piano Buzz

--- mysite/main/static/js/generateSong.js
+++ mysite/main/static/js/generateSong.js
@@ -1,4 +1,16 @@
 function test() {
-    console.log("here");
-    sampler.triggerAttackRelease("D6");
+    //console.log("here");
+    sampler.triggerAttackRelease(["C4", "E4", "G4"]);
+}
+
+function test2() {
+    sampler.triggerAttackRelease("C4");
+}
+
+function test3() {
+    sampler.triggerAttackRelease("E4");
+}
+
+function test4() {
+    sampler.triggerAttackRelease("G4");
 }
